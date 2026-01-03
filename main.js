2c70eadaaeccfc7b1748ee6c3785f5d1368c3240
Coco10023 <mauricio2003@live.se>
2026-01-03 17:45:27 +0100

    update

--- main.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('#primary-nav');
-
-menuToggle.addEventListener('click', () => {
-  const isOpen = nav.classList.toggle('is-open');
-  menuToggle.setAttribute('aria-expanded', isOpen);
-});
